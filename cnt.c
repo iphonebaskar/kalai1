89154a11b3834d2e97b0a6b6ce4d0eb789e32cd5
kalaipraba <35525844+kalaipraba@users.noreply.github.com>
2018-01-31 21:41:01 +0530

    Create cnt.c

---
+++ cnt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+    long long a;
+    int count = 0;
+
+    printf("Enter an integer: ");
+    scanf("%lld", &a);
+
+    while(n != 0)
+    {
+        // a = a/10
+        na/= 10;
+        ++count;
+    }
+
+    printf("Number of digits: %d", count);
+    getch();
+}
