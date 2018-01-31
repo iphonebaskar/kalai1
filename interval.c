32dd4fb2dd5a9828c2674d2497e77c88bc740689
kalaipraba <35525844+kalaipraba@users.noreply.github.com>
2018-01-31 22:29:18 +0530

    Create interval.c

---
+++ interval.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void printEvenOdd(int cur, int limit);
+int main()
+{
+    int lowerLimit;
+    printf("Enter lower limit: ");
+    scanf("%d", &lowerLimit);
+    
+    printf("Even/odd Numbers from %d to %d are: ", lowerLimit, upperLimit);
+    printEvenOdd("lowerLimit"); 
+    
+  getch();
+}
+
+
