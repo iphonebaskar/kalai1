ca410af83c1148cd7c7e335bb730cd872f19ee69
kalaipraba <35525844+kalaipraba@users.noreply.github.com>
2018-01-31 22:32:58 +0530

    Create pritwo.c

---
+++ pritwo.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int n, i, j, min, max, flag=0;
+    clrscr();
+    
+    printf("\n Enter two numbers(Intervals) : ");
+    scanf("%d%d",&min,&max);
+    
+    printf("\n The prime numbers between %d and %d are :",min,max);
+    
+    for(i=min+1;i<max;i++)
+    {
+        flag=0;
+        
+        for(j=2;j<=sqrt(i);j++)      //Loop to check whether 'i' (number) is divisible by any number between 2 and sqrt(i)
+        {
+            if(i%j==0)
+            {
+                flag=1;
+                break;
+            }
+        }
+        
+        /* if condition to check and print if the Number is Prime Number */
+        if (flag==0)
+        {
+            printf(" %d ",i);
+        }
+    }
+    return(0);
